9842da7c3c79caead8a1dd392db46b66969d97c6
Stissi <cs6556p@pace.edu>
2022-11-16 13:45:13 -0500

    lab 8

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,7 +11,17 @@ function addToCart(cookie) {
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
-   //add 1 to the itemCount variable
+   console.log(itemCount++) //add 1 to the itemCount variable
+
+   if(cookie == 'peanut butter')
+       currentPrice = currentPrice + 20;
+   if(cookie == 'sandies')
+       currentPrice = currentPrice + 30;
+   if(cookie == 'party press')
+       currentPrice = currentPrice + 35;
+   if(cookie == 'chocolate chip')
+       currentPrice = currentPrice + 25;
+   console.log(currentPrice);
    //add the correct price to the currentPrice variable
 
 }
@@ -19,5 +29,5 @@ function addToCart(cookie) {
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    
+    alert("Current Price="+addedToCart.getTotalCost+" itemCount="+addedToCart.getItemCount)
 }
